1c60e1077518414af60a7e1d9c9f97239eab2220
Vianney <vianney_mb@yahoo.fr>
2023-11-11 14:57:04 +0100

    Init Cackstage project : add CatalogPage and CatalogTable

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -33,6 +33,7 @@ import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
 import { CatalogGraphPage } from '@backstage/plugin-catalog-graph';
 import { RequirePermission } from '@backstage/plugin-permission-react';
 import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common/alpha';
+import { DefaultCatalogPage } from './components/CatalogPage';
 
 const app = createApp({
   apis,
@@ -58,7 +59,7 @@ const app = createApp({
 const routes = (
   <FlatRoutes>
     <Route path="/" element={<Navigate to="catalog" />} />
-    <Route path="/catalog" element={<CatalogIndexPage />} />
+    <Route path="/catalog" element={<DefaultCatalogPage />} />
     <Route
       path="/catalog/:namespace/:kind/:name"
       element={<CatalogEntityPage />}
